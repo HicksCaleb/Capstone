26e7308a691d7f769fb48c5bcbdc725191c61ef3
acuuser <acuuser@ENGI-LAT-02.acu.local>
2018-02-13 11:19:45 -0600

    lots of runs and some data

--- calibrate.py
+++ calibrate.py
@@ -10,13 +10,13 @@ width,height = 1024,768
 cam = uc480.uc480()
 
 screen = pygame.display.set_mode([width,height],pygame.FULLSCREEN)
-cut = 20
+cut = 15
 
 SLMPix = np.zeros((width,height))
 
 val = [x for x in range(0,256,1)]
 lines = [250,700]
-lines = [700,250]
+lines = [650,250]
 
 phase = []
 for a in val:
@@ -24,23 +24,27 @@ for a in val:
     draw = np.stack((SLMPix,)*3,axis=2)
     pygame.surfarray.blit_array(screen,draw)
     pygame.display.flip()
+    time.sleep(0.3)
     cam = uc480.uc480()
     cam.connect()
-    img = cam.acquire()
+    img = cam.acquire(1000)
     cam.disconnect()
     phi=[]
     peak = -1
     for b in lines:
-        row = img[b,:]
+        row = np.zeros(img.shape[1])
+        for c in range(100):
+            row = row+img[b+c,:]
+        #row = img[b,:]
         rowFT = np.fft.fft(row)
         rowFT[:cut] = 0
-        rowFT[40:] = 0
+        rowFT[30:] = 0
         if peak == -1:
             peak = np.argmax(np.abs(rowFT))
         print(peak)
         phiN = np.log(2*rowFT[peak]).imag
         phi.append(phiN)
-    dphi = phi[1]-phi[0]
+    dphi = phi[0]-phi[1]
     waveshift = dphi/(2*np.pi)
     phase.append(waveshift)
 for ca,a in enumerate(val):
--- plotter.py
+++ plotter.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys, os
 
-inf = 'out2.tsv'
+#inf = 'out4.tsv'
+inf = sys.argv[1]
 inFile = open(inf,'r')
-y = [float(line.split('\t')[1]) for line in inFile]
-plt.scatter([x for x in range(256)], y)
+y = [float(line.split('\t')[1])%1 for line in inFile]
+plt.scatter([x for x in range(len(y))], y)
 plt.show()
